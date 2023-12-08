44b5f667017fe4a9256dc71bffaf7a000ddd1a39
unknown <Ellie Nguyen>
2023-12-08 11:07:07 -0800

    Adds exceptions

--- ASLInterpreter.py
+++ ASLInterpreter.py
@@ -1,52 +1,57 @@
+# Loads a model to classify ASL from the webcam
+
 from keras.models import load_model  # TensorFlow is required for Keras to work
 import cv2  # Install opencv-python
 import numpy as np
 
-# Disable scientific notation for clarity
-np.set_printoptions(suppress=True)
+try: 
+    # Disable scientific notation for clarity
+    np.set_printoptions(suppress=True)
 
-# Load the model
-model = load_model("asl_model.h5", compile=False)
+    # Load the model
+    model = load_model("asl_model.h5", compile=False)
 
-# Load the labels
-class_names = open("label_code.txt", "r").readlines()
+    # Load the labels
+    class_names = open("label_code.txt", "r").readlines()
 
-# CAMERA can be 0 or 1 based on default camera of your computer
-camera = cv2.VideoCapture(0)
+    # CAMERA can be 0 or 1 based on default camera of your computer
+    camera = cv2.VideoCapture(0)
 
-while True:
-    # Grab the webcamera's image.
-    ret, image = camera.read()
+    while True:
+        # Grab the webcamera's image.
+        ret, image = camera.read()
 
-    # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+        # Resize the raw image into (224-height,224-width) pixels
+        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
+        # Show the image in a window
+        cv2.imshow("Webcam Image", image)
 
-    # Make the image a numpy array and reshape it to the models input shape.
-    image_array = np.asarray(image, dtype=np.float32)
-    image_array = np.resize(image_array, (1, 28, 28, 1))
+        # Make the image a numpy array and reshape it to the models input shape.
+        image_array = np.asarray(image, dtype=np.float32)
+        image_array = np.resize(image_array, (1, 28, 28, 1))
 
-    # Normalize the image array
-    normalized_image_array = (image_array / 127.5) - 1
+        # Normalize the image array
+        normalized_image_array = (image_array / 127.5) - 1
 
-    # Predicts the model
-    prediction = model.predict(normalized_image_array)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
+        # Predicts the model
+        prediction = model.predict(normalized_image_array)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
 
-    # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
-    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+        # Print prediction and confidence score
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
 
-    # Listen to the keyboard for presses.
-    keyboard_input = cv2.waitKey(1)
+        # Listen to the keyboard for presses.
+        keyboard_input = cv2.waitKey(1)
 
-    # 27 is the ASCII for the esc key on your keyboard.
-    if keyboard_input == 27:
-        break
+        # 27 is the ASCII for the esc key on your keyboard.
+        if keyboard_input == 27:
+            break
+except KeyboardInterrupt: 
+    camera.release()
+    cv2.destroyAllWindows()
+    print("error")
 
-camera.release()
-cv2.destroyAllWindows()
